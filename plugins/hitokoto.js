d3e9d53463656ef6f11cc32b4c3b8d67e97499b1
Runc2333 <814537405@qq.com>
2020-03-24 16:12:59 +0800

    Update hitokoto.js

--- plugins/hitokoto.js
+++ plugins/hitokoto.js
@@ -29,6 +29,13 @@ function init() {
         regex: "/(^一言|一言$|hitokoto)/",
         description: "获取一句话~"
     });
+    config.registerSuperCommand({
+        command: "hitokoto",
+        script: "hitokoto.js",
+        handler: "command",
+        argument: "[action]",
+        description: "[action]:\nenable|disable - 启用或禁用Hitokoto.#admin\n"
+    });
     if (config.get("HITOKOTO") === false) {
         var data = {};
         data["HITOKOTO_DISABLE_GROUPS"] = [];
@@ -38,10 +45,76 @@ function init() {
 }
 
 function hitokoto(packet) {
-    
+    var HITOKOTO_DISABLE_GROUPS = config.get("HITOKOTO", "HITOKOTO_DISABLE_GROUPS");
+    var index = HITOKOTO_DISABLE_GROUPS.indexOf(packet.group_id.toString());
+    if (index === -1) {
+        var url = "https://v1.hitokoto.cn/";
+        var res = request("GET", url);
+        try {
+            var response = JSON.parse(res.getBody("utf8"));
+        } catch (e) {
+            console.log(res.getBody("utf8"));
+            log.write("无法解析服务器返回的数据.", "HITOKOTO", "WARNING");
+            log.write("请检查后端服务器是否工作正常.", "HITOKOTO", "WARNING");
+            return false;
+        }
+        var msg = `${response.hitokoto} ————${response.from}`;
+        message.prepare(packet, msg, true).send();
+    }
+}
+
+function command(packet) {
+    var options = cqcode.decode(packet.message).pureText.split(" ");
+    switch (options[1]) {
+        case "enable":
+            /* 检查权限 */
+            if (packet.sender.role !== "admin") {
+                var msg = "[Hitokoto] 权限不足.";
+                message.prepare(packet, msg, true).send();
+                return false;
+            }
+            var HITOKOTO_DISABLE_GROUPS = config.get("HITOKOTO", "HITOKOTO_DISABLE_GROUPS");//读出配置文件里的已禁用群组
+            var index = HITOKOTO_DISABLE_GROUPS.indexOf(packet.group_id.toString());//判断是否已经禁用
+            if (index !== -1) {
+                //处于禁用状态
+                HITOKOTO_DISABLE_GROUPS.splice(index, 1);
+                config.write("HITOKOTO", HITOKOTO_DISABLE_GROUPS, "HITOKOTO_DISABLE_GROUPS");
+                var msg = "[Hitokoto] 已启用.";
+            } else {
+                //处于启用状态
+                var msg = "[Hitokoto] 已经是启用状态了, 无需重复启用.";
+            }
+            message.prepare(packet, msg, true).send();
+            break;
+        case "disable":
+            if (packet.sender.role !== "admin") {
+                var msg = "[Hitokoto] 权限不足.";
+                message.prepare(packet, msg, true).send();
+                return false;
+            }
+            var HITOKOTO_DISABLE_GROUPS = config.get("HITOKOTO", "HITOKOTO_DISABLE_GROUPS");//读出配置文件里的已禁用群组
+            var index = HITOKOTO_DISABLE_GROUPS.indexOf(packet.group_id.toString());//判断是否已经禁用
+            if (index === -1) {
+                //处于启用状态
+                HITOKOTO_DISABLE_GROUPS.push(packet.group_id.toString());
+                config.write("HITOKOTO", HITOKOTO_DISABLE_GROUPS, "HITOKOTO_DISABLE_GROUPS");
+                var msg = "[Hitokoto] 已禁用.";
+            } else {
+                //处于禁用状态
+                var msg = "[Hitokoto] 已经是禁用状态了, 无需重复禁用.";
+            }
+            message.prepare(packet, msg, true).send();
+            break;
+        default:
+            log.write("处理失败:未知指令.", "HITOKOTO", "WARNING");
+            var msg = "[Hitokoto] 未知指令.";
+            message.prepare(packet, msg, true).send();
+            return false;
+    }
 }
 
 module.exports = {
     init,
-    hitokoto
+    hitokoto,
+    command
 }
